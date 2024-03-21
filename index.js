b8d8b0f73aa8ceb101b50c66f836be7b5e8c1e3b
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-03-21 05:59:26 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        let message = Thankyou, ${names[i]}, for the wonderful ${eventName} gift!;
+        messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;
+    while (currentNumber >= 0) {
+        console.log(currentNumber);
+        currentNumber--;
+    }
+}
+
+countDown(10);
